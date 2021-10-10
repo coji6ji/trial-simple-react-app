0247eaae6c8cce7ed90257d32dd09c17fbe7cff2
araihara-k <araihara@gmail.com>
2021-10-10 13:35:37 +0900

    feat: add onClick envets to transition routes

--- src/components/molcules/MenuDrawer.tsx
+++ src/components/molcules/MenuDrawer.tsx
@@ -10,18 +10,27 @@ import { Button } from "@chakra-ui/button";
 type Props = {
   isOpen: boolean;
   onClose: () => void;
+  onClickHome: () => void;
+  onClickUserManagement: () => void;
+  onClickSetting: () => void;
 };
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { isOpen, onClose } = props;
+  const { isOpen, onClose, onClickHome, onClickUserManagement, onClickSetting } = props;
   return (
     <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg="gray.100">
-            <Button w="100%">HOME</Button>
-            <Button w="100%">ユーザー一覧</Button>
-            <Button w="100%">設定</Button>
+            <Button onClick={onClickHome} w="100%">
+              HOME
+            </Button>
+            <Button onClick={onClickUserManagement} w="100%">
+              ユーザー一覧
+            </Button>
+            <Button onClick={onClickSetting} w="100%">
+              設定
+            </Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,11 +1,18 @@
-import { memo, VFC } from "react";
+import { memo, useCallback, VFC } from "react";
 import { Box, Flex, Heading, Link } from "@chakra-ui/layout";
 import { useDisclosure } from "@chakra-ui/hooks";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molcules/MenuDrawer";
+import { useHistory } from "react-router";
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
+
+  const onClickHome = useCallback(() => history.push("/home"), []);
+  const onClickUserManagement = useCallback(() => history.push("/home/user_management"), []);
+  const onClickSetting = useCallback(() => history.push("/home/setting"), []);
+
   return (
     <>
       <Flex
@@ -16,25 +23,26 @@ export const Header: VFC = memo(() => {
         justify="space-between"
         padding={{ base: 3, md: 5 }}
       >
-        <Flex as="a" align="center" mr={8} _hover={{ cursor: "pointer" }}>
+        <Flex as="a" align="center" mr={8} _hover={{ cursor: "pointer" }} onClick={onClickHome}>
           <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
             ユーザー管理アプリ
           </Heading>
         </Flex>
-        <Flex
-          align="center"
-          fontSize="sm"
-          flexGrow={2}
-          display={{ base: "none", md: "flex" }}
-        >
+        <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
           <Box pr={4}>
-            <Link>ユーザー管理</Link>
-            <Link>設定</Link>
+            <Link onClick={onClickUserManagement}>ユーザー管理</Link>
           </Box>
+          <Link onClick={onClickSetting}>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <MenuDrawer onClose={onClose} isOpen={isOpen} />
+      <MenuDrawer
+        onClose={onClose}
+        isOpen={isOpen}
+        onClickHome={onClickHome}
+        onClickUserManagement={onClickUserManagement}
+        onClickSetting={onClickSetting}
+      />
     </>
   );
 });
