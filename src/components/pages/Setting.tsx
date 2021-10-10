e1f2065b6ce24426807d3c0f801411e407b4c2e3
araihara-k <araihara@gmail.com>
2021-10-10 12:20:17 +0900

    feat: after login pages and 404

--- src/components/pages/Setting.tsx
+++ src/components/pages/Setting.tsx
@@ -1,5 +1,5 @@
 import { memo, VFC } from "react";
 
-export const Home: VFC = memo(() => {
+export const Setting: VFC = memo(() => {
   return <p>設定</p>;
 });
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,5 @@
 import { memo, VFC } from "react";
 
-export const Home: VFC = memo(() => {
+export const UserManagement: VFC = memo(() => {
   return <p>ユーザー管理</p>;
 });
