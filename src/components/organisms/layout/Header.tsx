f365766c9fc983db2a75d1d3564c1688c18a130a
araihara-k <araihara@gmail.com>
2021-10-10 13:53:01 +0900

    feat: Implements Login latout

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -17,7 +17,7 @@ export const Header: VFC = memo(() => {
     <>
       <Flex
         as="nav"
-        bg="teal.500"
+        bg="blue.500"
         color="gray.50"
         align="center"
         justify="space-between"
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,20 @@
+import { Flex, Button, Box, Heading, Divider, Input, Stack } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
-  return <p>ログインページ</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="md" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={4} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <PrimaryButton>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
