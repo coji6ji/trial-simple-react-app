f4e6253a0e5e7a2bdf538fdff2bd731eb7c9282a
araihara-k <araihara@gmail.com>
2021-10-10 12:02:51 +0900

    feat: default routing setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme/theme";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router/Router";
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider theme={theme}>
-        <Button colorScheme="teal">ボタン</Button>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </ChakraProvider>
     </div>
   );
---
+++ src/components/pages/UserManagement.tsx
@@ -0,0 +1,5 @@
+import { memo, VFC } from "react";
+
+export const Home: VFC = memo(() => {
+  return <p>ユーザー管理</p>;
+});
