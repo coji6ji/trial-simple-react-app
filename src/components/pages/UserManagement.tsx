cea6e6213d3cacf7e8e9dcbb4c775340523cc52e
araihara-k <araihara@gmail.com>
2021-10-10 17:23:43 +0900

    feat: enabled user editting feature when logined as admin user

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -43,7 +43,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
