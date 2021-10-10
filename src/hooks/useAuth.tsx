c21236e699721d4a4c13ff4e3c85cc112fd769fd
araihara-k <araihara@gmail.com>
2021-10-10 14:39:47 +0900

    feat: add hooks to show toast with chakra-ui

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -2,9 +2,11 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 import { User } from "../types/api/user";
 import { useHistory } from "react-router";
+import { useMessage } from "./userMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false);
   const login = useCallback(
     (userId: string) => {
@@ -13,15 +15,16 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`)
         .then((response) => {
           if (response.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
-            alert("ユーザーが見つかりません");
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
           }
         })
-        .catch((error) => alert("ログインできません"))
+        .catch((error) => showMessage({ title: "ログインできません", status: "error" }))
         .finally(() => setLoading(false));
     },
-    [history]
+    [history, showMessage]
   );
 
   return { login, loading };
